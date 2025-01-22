01cd3a16bd5073e93a17a42d3dd333f67ac7d271
esosavillalba <esosa@taxit.com.py>
2025-01-22 17:32:58 -0300

    Commit inicial de prueba tecnica

---
+++ src/utils/logger.ts
@@ -0,0 +1,15 @@
+export const LogInfo = (message : string) => {
+    console.log(`INFO -> ${message}`);
+}
+
+export const LogSuccess = (message : string) => {
+    console.log(`SUCCESS -> ${message}`);
+}
+
+export const Logwarning = (message : string) => {
+    console.log(`WARN -> ${message}`);
+}
+
+export const LogError = (message : string) => {
+    console.log(`ERROR -> ${message}`);
+}
